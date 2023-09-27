70603253b643c7216071781cdfa10958f222de7d
AndreBlankholm <ablankholm@gmail.com>
2023-09-26 19:23:11 -0500

    created a template for a recipe item for the recipe model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
-
+recipes: Recipe[] = [
+  new Recipe("Rice", "the best asian rice", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcooking.nytimes.com%2Frecipes%2F1019204-basic-stovetop-rice&psig=AOvVaw3Ew7StlA_N5_PC3Xm6x5-l&ust=1695859805357000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCOjN-pvAyYEDFQAAAAAdAAAAABAE")
+];
 }
